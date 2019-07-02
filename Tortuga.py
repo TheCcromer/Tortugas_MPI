9609e2d21d29806a21cc258a1507327138c7206d
fjimenez1999 <fjimenez1999@hotmail.com>
2019-07-01 20:39:07 -0600

    Update Tortuga.py

--- Tortuga.py
+++ Tortuga.py
@@ -10,7 +10,8 @@ class Tortuga:
 	
 	## VARIABLES DE CLASE
 	id = 0 		## OJO variable static de clase para generar ids de tortugas
-	
+	duraciones = [] ## variable para saber cuanto dura cada estado de cada tortuga a partir de camar
+	duracion_actual = 0
 	## MÉTODOS DE CLASE
 	""" metodo de clase que genera N tortugas """
 	@classmethod
@@ -64,6 +65,72 @@ class Tortuga:
 		self.posicion = pn
 		return
 		
+	## hay que ver si ubicar este metodo en la clase tortuga o en la clase del main
+	## devuelve un booleano que es true si se desactivo la tortuga o false si no se desactivo
+	@classmethod
+	def proba_desactivarse(self, archivo):
+		probabilidad  = np.random.uniform(low = 0.0, high = 1.0, size = None)
+		desactivada = False
+		# remplazar los valores de probabilidad necesarios para cambiar de estado por lo que se va a leer de los archivos
+		if(probabilidad <= 0.2 and self.estado == Tortuga.EstadoTortuga.vagar):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		elif(probabilidad <= 0.2 and self.estado == Tortuga.EstadoTortuga.camar):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		elif(probabilidad <= 0.6 and self.estado == Tortuga.EstadoTortuga.excavar):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		elif(probabilidad <= 0.2 and self.estado == Tortuga.EstadoTortuga.poner):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		elif(probabilidad <= 0.01 and self.estado == Tortuga.EstadoTortuga.tapar):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		elif((probabilidad <= 0.01 and self.estado == Tortuga.EstadoTortuga.camuflar):
+			self.estado = Tortuga.EstadoTortuga.inactiva
+			desactivada = True
+		return desactivada
+	
+	## sacar los datos de distribucion normal y el 372 de archivos
+	## Inicializa las duraciones de cada estado de cada tortuga
+	@classmethod
+	def inicializar_duraciones(self, archivo):
+		duracion_camar = np.random.normal(1.58,1.44,None) #distribucion normal de camar
+		duracion_excavar = np.random.normal(12.35,4.92,None) #distribucion normal de excavar
+		duracion_poner = np.random.normal(11.64,4.34,None) #distribucion normal de poner
+		duracion_tapar = np.random.normal(4.98,3.74,None) #distribucion normal de tapar
+		duracion_camuflar = np.random.normal(5.01,1.81,None) #distribucion normal de camuflar
+		duracion_vagar = 372-(duracion_camar+duracion_excavar+duracion_poner+duracion_tapar+duracion_camuflar) 
+		duraciones.append(duracion_vagar) #duracion de vagar
+		duraciones.append(duracion_camar) #duracion de camar, etc...
+		duraciones.append(duracion_excavar) 
+		duraciones.append(duracion_poner) 
+		duraciones.append(duracion_tapar)	
+		duraciones.append(duracion_camuflar) 
+		
+	## este metodo esta directamente relacionado a avanzar y la desviacion estandar de la duracion de cada estado
+	## la desviacion estandar es algo diferente para cada tortuga
+	@classmethod
+	def cambiar_estado(self, archivo):
+		if(!proba_desactivarse(self, archivo)):
+			if(self.estado == Tortuga.EstadoTortuga.vagar and duracion_actual >= duraciones[0]):
+				self.estado = Tortuga.EstadoTortuga.camar
+				duracion_actual = 0
+			elif(self.estado == Tortuga.EstadoTortuga.camar and duracion_actual >= duraciones[1]):
+				self.estado = Tortuga.EstadoTortuga.excavar
+				duracion_actual = 0
+			elif(self.estado == Tortuga.EstadoTortuga.excavar and duracion_actual >= duraciones[2]):
+				self.estado = Tortuga.EstadoTortuga.poner
+				duracion_actual = 0
+			elif(self.estado == Tortuga.EstadoTortuga.poner and duracion_actual >= duraciones[3])
+				self.estado = Tortuga.EstadoTortuga.tapar
+				duracion_actual = 0
+			elif(self.estado == Tortuga.EstadoTortuga.tapar and duracion_actual >= duraciones[4])
+				self.estado = Tortuga.EstadoTortuga.camuflar
+				
 	## EFE: avanza la tortuga de acuerdo con su estado
 	def avanzar(self):
+		#duracion_actual++
 		return
+	
